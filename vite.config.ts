29e9153764184606f2a20be0d4ac10b269fbfb4b
popcornstudiosx-ui <popcornstudiosx@gmail.com>
2026-06-27 18:01:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: './client', // Add this line! It tells Vite to look inside the /client folder
   server: {
     host: true,
     proxy: {
